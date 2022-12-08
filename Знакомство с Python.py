23eb0ae7b6164508b374149026f1a518be0c086e
LuchnikovAnton <120092616+LuchnikovAnton@users.noreply.github.com>
2022-12-08 15:32:35 +0300

    Add files via upload

---
+++ Знакомство с Python.py
@@ -0,0 +1,33 @@
+print('Задание 1:')
+data=input("Введите дату: ")
+task=input('Введите задачу: ')
+print(data,task)
+
+print('Задание 2:')
+data1=input("Введите дату: ")
+task1=input('Введите задачу: ')
+data2=input("Введите дату: ")
+task2=input('Введите задачу: ')
+data3=input("Введите дату: ")
+task3=input('Введите задачу: ')
+print(data1,task1)
+print(data2,task2)
+print(data3,task3)
+
+print('Задание 3:')
+data4=input("Введите дату: ")
+task4=input('Введите задачу: ')
+data5=input("Введите дату: ")
+task5=input('Введите задачу: ')
+data6=input("Введите дату: ")
+task6=input('Введите задачу: ')
+slovary={'data4':"task4","data5":"task5","data6":"task6"}
+
+
+
+
+
+
+
+
+
